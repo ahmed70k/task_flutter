85c75d270e4897a4ab14a05c7a9e4fe282a44acc
Ahmed Alaa <ahmedalaa575370@gmail.com>
2026-01-15 13:39:41 +0200

    add : initial project structure

---
+++ lib/features/admin/ui/admin_screen/admin_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AdminScreen extends StatelessWidget {
+  static const String routeName = "AdminScreen";
+  const AdminScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
